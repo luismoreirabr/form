373df248e4ebda1f7c7aba0d2ad3e8594ab2504c
Luis Moreira <luismoreira294@outlook.com>
2023-08-20 20:13:31 -0300

    add js and modal

---
+++ src/js.js
@@ -0,0 +1,47 @@
+// const showModalButton = document.getElementById("showModalBtn");
+//   const modal = document.getElementById("successModal");
+
+//   showModalButton.addEventListener("click", () => {
+//     modal.classList.remove("hidden");
+//     modal.classList.add("flex");
+//   });
+
+//   // Dismiss modal when "Dismiss message" is clicked
+//   const dismissButton = document.getElementById("btnsubs");
+//   dismissButton.addEventListener("click", () => {
+//     modal.classList.add("hidden");
+//   });
+
+
+
+
+
+// function validarEmail() {
+//     var email = document.querySelector('#email');
+//     var error = document.querySelector('#error-email');
+
+//     if (!email.checkValidity()) {
+//         error.innerHTML = "Email invalido";
+//     }
+
+// }
+
+// validarEmail();
+
+// function redefinirMsg() {
+//     var error = document.querySelector('#error-email');
+//     if (error.innerHTML == "Email invalido") {
+//         error.innerHTML = "";
+//     }
+// }
+
+// console.log("win")
+
+// function modalNews() {
+//     const btnAddModal = document.getElementById("btnsub");
+//     const modalOpen = document.getElementById("successModal");
+//     btnAddModal.addEventListener("click", () => {
+//       modalOpen.classList.add("!flex");
+//     });
+    
+//   }
